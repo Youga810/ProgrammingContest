cc7526db1c4ce19740b76c7c2f738fd0e0c7f746
pron.youg810@gmail.com <pron.youg810@gmail.com>
2021-03-05 23:57:11 +0900

    on the way

--- 192/C.py
+++ 192/C.py
@@ -1,11 +1,34 @@
-N, K = map(int, input().split())
-
-
-for i in range(K):
-
-
 def maximum(x):
     lst = []
-    while x / 10 < 1:
-        x = x/10
-        lst.append(x)
+    while x >= 1:
+        rest = x % 10
+        x /= 10
+        lst.append(int(rest))
+    lst.sort()
+    print(lst)
+    maximum = 0
+    for i in range(len(lst)):
+        maximum += lst[i] * (10 ** i)
+    return maximum
+
+
+def minimum(x):
+    lst = []
+    while x >= 1:
+        rest = x % 10
+        x /= 10
+        lst.append(int(rest))
+    lst.sort(reverse=True)
+    print(lst)
+    minimum = 0
+    for i in range(len(lst)):
+        minimum += lst[i] * (10 ** i)
+    return minimum
+
+
+N, K = map(int, input().split())
+
+x = N
+for i in range(K):
+    x = maximum(x) - minimum(x)
+print(x)
