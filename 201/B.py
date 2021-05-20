e1886b154015eea07417fc56e7f9d4b8da4593ea
pron.youg810@gmail.com <pron.youg810@gmail.com>
2021-05-21 00:22:59 +0900

    【Add】完成版(????)

--- 201/B.py
+++ 201/B.py
@@ -1,7 +1,5 @@
-import operator
-
 n = int(input())
 st = [input().split() for j in range(n)]
 
-sortedItem = sorted(st, key=operator.itemgetter(1))
-print(sortedItem[-2][0])
+sortedItem = sorted(st, key=lambda x: int(x[1]), reverse=True)
+print(sortedItem)
