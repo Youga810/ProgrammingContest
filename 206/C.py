3d4b32a20f2b9cf1e5205bed82eaa43bc6c9a1c8
pron.youg810@gmail.com <pron.youg810@gmail.com>
2021-06-20 02:39:24 +0900

    solved

--- 206/C.py
+++ 206/C.py
@@ -1,19 +1,13 @@
-import itertools
 n = int(input())
 a = list(map(int, input().split()))
 a = sorted(a)
-sum = 0
-lst = []
-# for i in range(1, n):
-#    if a.count(i) > 1:
-#        lst.append(a.count(i))
-#    sum += i
-# for i in lst:
-#    cnt = 0
-#
-#    sum -= cnt
-# print(sum)
-#
-
-for balls in itertools.combinations(a, 2):
-    print(balls)
+sum = int(n * (n-1) / 2)
+b = {}
+for i in range(n):
+    if a[i] not in b:
+        b[a[i]] = 1
+    else:
+        b[a[i]] += 1
+for val in b.values():
+    sum -= int(val * (val-1) / 2)
+print(sum)
