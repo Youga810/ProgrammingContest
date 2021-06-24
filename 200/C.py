3fb7b3a9d7dd6a2740a9114ecdc8b78b99ad8652
pron.youg810@gmail.com <pron.youg810@gmail.com>
2021-06-25 00:23:11 +0900

    solved

--- 200/C.py
+++ 200/C.py
@@ -2,3 +2,15 @@ n = int(input())
 a = list(map(int, input().split()))
 
 a = sorted(a)
+a = [i % 200 for i in a]
+s = dict()
+for i in a:
+    if i not in s:
+        s[i] = 1
+    else:
+        s[i] += 1
+sum = 0
+for key, val in s.items():
+    sum += int(val * (val-1) / 2)
+
+print(sum)
