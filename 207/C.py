5be772fd7d439cccb617b6e75693f8ffd9f646a5
pron.youg810@gmail.com <pron.youg810@gmail.com>
2021-06-30 23:01:41 +0900

    solved

--- 207/C.py
+++ 207/C.py
@@ -1,7 +1,7 @@
 n = int(input())
 lst = []
 for i in range(n):
-    t,l,r = map(int,input().split())
+    t, l, r = map(int, input().split())
     if t == 2:
         r -= 0.1
     elif t == 3:
@@ -9,10 +9,10 @@ for i in range(n):
     elif t == 4:
         r -= 0.1
         l += 0.1
-    lst.append([l,r])
+    lst.append([l, r])
 cnt = 0
 for i in range(n):
-    for j in range(i+1,n):
-        if lst[i][1] >= lst[j][0]:
+    for j in range(i+1, n):
+        if min(lst[i][1], lst[j][1]) >= max(lst[j][0], lst[i][0]):
             cnt += 1
 print(cnt)
