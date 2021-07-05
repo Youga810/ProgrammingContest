17469eb07320f64f308f3aaec8a8bb41aea00fe0
pron.youg810@gmail.com <pron.youg810@gmail.com>
2021-07-05 23:02:26 +0900

    solved

--- 208/C.py
+++ 208/C.py
@@ -1,10 +1,11 @@
+import math
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 sorted_a = sorted(a)
 a_dict = {}
 rest = k % n
 for i in a:
-    a_dict[i] = int(k/n)
+    a_dict[i] = math.floor(k/n)
 for i in sorted_a:
     if rest == 0:
         break
