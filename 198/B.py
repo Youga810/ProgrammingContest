05d44595a76f49852f1486245f7cf43d27053d85
pron.youg810@gmail.com <pron.youg810@gmail.com>
2021-07-08 23:57:55 +0900

    ontheway

--- 198/B.py
+++ 198/B.py
@@ -1,5 +1,17 @@
 def palindrome(n):
+    for i in range(int(len(n) / 2)):
+        if n[i] != n[-1]:
+            return False
+    return True
 
 
 n = input()
 n_len = len(n)
+for i in range(0, 9):
+    flag = palindrome('0'*i + n)
+    if flag:
+        break
+if flag:
+    print('Yes')
+else:
+    print('No')
